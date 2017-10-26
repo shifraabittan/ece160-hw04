1c493ccb95cc1bd0bfe43915c7a8538e46c16f08
jordanklahr <jordanklahr@gmail.com>
2017-10-25 21:00:45 -0400

    commit

--- hw04.c
+++ hw04.c
@@ -18,18 +18,22 @@ typedef struct {
 
 void print_student(Mode m, Student s) {
   switch (m) {
-    case NAME_ONLY:
+    int calculated_years;
+	case NAME_ONLY:
       printf("%s %s\n", s.first_name, s.last_name);
       break;
 	case MAJOR_AND_NAME:
 	    printf("%s %s %s\n", s.major_field, s.first_name, s.last_name);
 	    break;
 	 case YEAR_AND_NAME:
-	    printf("%s %s %s\n", s.year, s.first_name, s.last_name);
+	    printf("%d %s %s\n", s.year, s.first_name, s.last_name);
 	    break;
-	 //case YEARS_LEFT_AND_NAME:
+	 case YEARS_LEFT_AND_NAME:
+		calculated_years = s.year - 2017;
+		printf("%d %s %s\n", calculated_years, s.first_name, s.last_name);
+		break;
 		
-		printf("")
+		
 	
   }
 }
@@ -41,7 +45,40 @@ void print_student(Mode m, Student s) {
 	except year is a number (int)
 */
 int main(int argc, char* argv[]) {
-
+	//identify name of file, assign file text to a pointer
+	char *filename = argv[1];
+	FILE *fp;
+	fp = fopen(filename, "r");
+	
+	//dummy variable to be used when parsing file lines
+	char buffer[256];
+	
+	//assign an array to hold all of the student info extracted from the file
+	Student all_students[256];
+	
+	
+	if (fp == NULL) {
+		printf("Could not open %s\n", filename);
+		return -1;
+	}
+	
+	//read one line at a time from file and assign it to the student structs 
+  
+	char *s;
+	int i = 0;
+	while ((s = fgets(buffer, 255, fp)) != NULL) {
+		
+		Student s1;
+		s1.first_name = buffer[0];
+		s1.last_name = s[1];
+		s1.major_field = s[2];
+		s1.year = atof(s[3]);
+		
+		all_students[i] = s1;
+		//sscanf (s from strings, f from files)
+		//call the function and print it back out
+	}
+  
   for (int i = 2; i < argc; i = i+4){
   int mode_value;
   mode_value = atof(argv[1]);
@@ -50,7 +87,8 @@ int main(int argc, char* argv[]) {
   s1.first_name = argv[i];
   s1.last_name = argv[i+1];
   s1.major_field = argv[i+2];
-  s1.year = argv[i+3];
+  
+  s1.year = atof(argv[i+3]);
   
   print_student(mode_value, s1);
   }
